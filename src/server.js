464d77b073ec7e90d86b20f0b75f4dde1b604b81
Vikas Yadav <vikasthapar95@gmail.com>
2026-01-27 13:40:28 +0530

    fix railway port hadnling

--- src/server.js
+++ src/server.js
@@ -1,10 +1,23 @@
-import dotenv from 'dotenv';
-dotenv.config();
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+// import app from './app.js';
+
+// const PORT = process.env.PORT || 4000;
+
+// app.listen(PORT, () => {
+//     console.log(`🚀 Piniffy backend running on port ${PORT}`);
+// });
+
+if (process.env.NODE_ENV !== 'production') {
+  const dotenv = await import('dotenv');
+  dotenv.config();
+}
 
 import app from './app.js';
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`🚀 Piniffy backend running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Piniffy backend running on port ${PORT}`);
 });
