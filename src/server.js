456b79f10bbfa864186fdd64f4ad912b3a89bfe4
Vikas Yadav <vikasthapar95@gmail.com>
2026-01-27 19:36:38 +0530

    PORT REMOVED

--- src/server.js
+++ src/server.js
@@ -10,14 +10,14 @@
 // });
 
 if (process.env.NODE_ENV !== 'production') {
-  const dotenv = await import('dotenv');
-  dotenv.config();
+    const dotenv = await import('dotenv');
+    dotenv.config();
 }
 
 import app from './app.js';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`🚀 Piniffy backend running on port ${PORT}`);
+    console.log(`🚀 Piniffy backend running on port ${PORT}`);
 });
